28506d1a2185318fc3ad5d58e9f167b235758c09
Yueci Sun <yuecisun289@gmail.com>
2024-09-20 16:38:16 -0400

    add front end auth

--- src/App.js
+++ src/App.js
@@ -1,39 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback} from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'; 
+
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 // import NewPlaceTest from './places/pages/NewPlaceTest';
 import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
+import { AuthContext } from './shared/context/auth-context';
+import './App.css';
 
-const App =()=> {
+const App = () => {
+  const [isLoggedin, setIsLoggedIn] = useState(false); 
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []); 
+  
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []); 
+  
+  let routes; 
+
+  if (isLoggedin) {
+    routes = (
+      <Switch>
+        <Route path='/' exact>
+          <Users />
+        </Route>
+
+        <Route path='/:userId/places' exact>
+          <UserPlaces />
+        </Route>
+
+        <Route path='/places/new' exact>
+          <NewPlace />
+        </Route>
+
+        <Route path='/places/:placeId' exact>
+          <UpdatePlace />
+        </Route>
+
+        <Redirect to="/" />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path='/' exact>
+          <Users />
+        </Route>
+
+        <Route path='/:userId/places' exact>
+          <UserPlaces />
+        </Route>
+
+        <Route path='/auth' exact>
+          <Auth />
+        </Route>
+
+        <Redirect to="/auth" />
+      </Switch>
+    );
+  }
+
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Switch>
-          <Route path='/' exact>
-            <Users />
-          </Route>
-        
-          <Route path='/:userId/places' exact>
-            <UserPlaces />
-          </Route>
-          
-          <Route path='/places/new' exact>
-            <NewPlace />
-          </Route>
-
-          <Route path='/places/:placeId' exact>
-            <UpdatePlace />
-          </Route>
-        
-          <Redirect to="/" />
-        </Switch>
-      </main>
-  </Router>
+    <AuthContext.Provider value={{isLoggedin: isLoggedin, login: login, logout:logout }}>
+      <Router>
+        <MainNavigation />
+        <main>
+          <Switch>{ routes}</Switch>
+        </main>
+        </Router>
+      </AuthContext.Provider>
   );
 }
 
--- src/shared/hooks/form-hook.js
+++ src/shared/hooks/form-hook.js
@@ -5,11 +5,14 @@ const formReducer = (state, action) => {
         case 'INPUT_CHANGE':
             let formIsValid = true;
             for (const inputId in state.inputs) {
-            if (inputId === action.inputId) {
-                formIsValid = formIsValid && action.isValid;
-            } else {
-                formIsValid = formIsValid && state.inputs[inputId].isValid;
-            }
+                if (!state.inputs[inputId]) {
+                    continue;
+                }
+                if (inputId === action.inputId) {
+                    formIsValid = formIsValid && action.isValid;
+                } else {
+                    formIsValid = formIsValid && state.inputs[inputId].isValid;
+                }
             }
             return {
                 ...state,
